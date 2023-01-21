0ea297dd2dc99f08a091df0964fd8c8b2ad6cedb
Rimaansh <67331016+Rimaansh@users.noreply.github.com>
2023-01-22 00:00:33 +0530

    Initial commit. Firebase Added to the App.

---
+++ lib/eventsPage.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class EventsPage extends StatefulWidget {
+  const EventsPage({Key? key}) : super(key: key);
+
+  @override
+  _EventsPageState createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  final List<String> _events = [
+    "Case Maze",
+    "Startup Mela",
+    "Event 3",
+    "Event 4",
+    "Event 5",
+    "Event 6",
+    "Event 7",
+    "Event 8",
+    "Event 9",
+    "Event 10",
+    "Event 11",
+    "Event 12",
+    "Event 13",
+    "Event 14",
+    "Event 15"
+  ];
+
+  final Set<String> _selectedEvents = Set();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+            childAspectRatio: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemCount: _events.length,
+        itemBuilder: (BuildContext context, int index) {
+          final event = _events[index];
+          return Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.blueAccent, borderRadius: BorderRadius.circular(10)),
+            child: ListTile(
+              title: Text(event, style: TextStyle(color: Colors.white),),
+              trailing: IconButton(
+                icon: _selectedEvents.contains(event)
+                    ? Icon(Icons.check_box)
+                    : Icon(Icons.check_box_outline_blank_outlined),
+                onPressed: () {
+                  setState(() {
+                    if (_selectedEvents.contains(event)) {
+                      _selectedEvents.remove(event);
+                    } else {
+                      _selectedEvents.add(event);
+                    }
+                  });
+                },
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.red,
+        label: Text("Register Now"),
+        icon: Icon(Icons.app_registration),
+        onPressed: () {
+          print("Selected events: $_selectedEvents");
+        },
+      ),
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'eventsPage.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.redAccent, Colors.grey],
+            ),
+          ),
+        child: EventsPage(),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          leading: Container(
+            padding: EdgeInsets.all(20),
+            width: 200,
+            height: 200,
+            decoration: const BoxDecoration(
+              // ignore: unnecessary_const
+              image: const DecorationImage(
+                image: AssetImage('images/meslogo.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ), //mes logo
+          leadingWidth: 60,
+          title: Row(
+            children: const [
+              Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0)),
+              Expanded(
+                child: Text(
+                  '***id of the user***',
+                  style: TextStyle(fontSize: 15),
+                ), // middle text
+              ),
+              Icon(
+                Icons.account_circle,
+                size: 40,
+                color: Colors.white,
+              ), // right icon
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
